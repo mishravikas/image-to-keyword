7bc97ba187245a702a0101d59683580880542506
Vikas Mishra <vikasmishra95@gmail.com>
2017-04-26 13:45:26 +0530

    Added code for nouns

--- my_script.py
+++ my_script.py
@@ -1,4 +1,13 @@
 import sys, json
+import nltk
+lines = ""
 
 for v in sys.argv[1:]:
-	print v
+	# print v
+	lines = v
+
+is_noun = lambda pos: pos[:2] == 'NN'
+# do the nlp stuff
+tokenized = nltk.word_tokenize(lines)
+nouns = [word for (word, pos) in nltk.pos_tag(tokenized) if is_noun(pos)]
+print nouns
